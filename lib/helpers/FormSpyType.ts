777dd07ca33bc849aa5d8c2ece83de6be45d42d3
Igor Kamyshev <igor@kamyshev.me>
2019-01-23 16:38:41 +0300

    fix: make defaultProps optional in FormSpyType

--- lib/helpers/FormSpyType.ts
+++ lib/helpers/FormSpyType.ts
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { FormSpyProps } from 'react-final-form'
 
 interface Additional {
-  defaultProps: any
+  defaultProps?: any
 }
 
 type FormSpyType = React.ComponentType<FormSpyProps> & Additional
